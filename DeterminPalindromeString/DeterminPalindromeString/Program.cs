e40dd80d41c74fd39234af0e5d7d81c250230e06
orakfarzaneh@gmail.com <orakfarzaneh@gmail.com>
2019-03-17 17:39:38 -0700

    Added new projet for detecting polindrome string

---
+++ DeterminPalindromeString/DeterminPalindromeString/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DeterminPalindromeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "a tacocata";
+            bool result = FindPalidromeString(str);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+        private static bool FindPalidromeString(string str)
+        {
+            bool isPalindrome = true; ;
+            for (int i = 0; i < str.Length / 2 ; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    str = str.Remove(i, 1);
+                    i--;
+                }
+                if(str[str.Length - 1 - i] == ' ')
+                {
+                    str = str.Remove(str.Length - 1 - i, 1);
+                    i--;
+                }
+                if (str[i] != str[str.Length -1 -i] )
+                {
+                    isPalindrome = false;
+                    break;
+                }
+                
+            }
+            Console.WriteLine(str);
+            return isPalindrome;
+        }
+    }
+}
