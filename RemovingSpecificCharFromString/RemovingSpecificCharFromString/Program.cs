f72bc800b64d18406f7d6387dca42d9cc29039a3
orakfarzaneh@gmail.com <orakfarzaneh@gmail.com>
2019-03-17 17:12:33 -0700

    added new project for removing a char from a string

---
+++ RemovingSpecificCharFromString/RemovingSpecificCharFromString/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+namespace renoveGivenChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "abcadae";
+            string expectedResult = "bcde";
+            char removeChar = 'a';
+            string result = CharacterRemover(str, removeChar);
+            if (result == expectedResult)
+            {
+                Console.WriteLine("True");
+                Console.WriteLine(result);
+            }
+
+            string result2 = characterRemovalWithoutStringBuilder(str, removeChar);
+            if (result2 == expectedResult)
+            {
+                Console.WriteLine("True");
+                Console.WriteLine(result2);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static string CharacterRemover(string str, char removeChar)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != removeChar)
+                {
+                    stringBuilder.Append(str[i]);
+
+                }
+            }
+            return stringBuilder.ToString();
+        }
+
+        private static string characterRemovalWithoutStringBuilder(string str, char removeChar)
+        {
+            if (str.Length == 0)
+            {
+                return null;
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == removeChar)
+                {
+                    str = str.Remove(i, 1);
+                }
+            }
+            return str;
+
+        }
+    }
+}
