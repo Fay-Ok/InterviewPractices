9b7c93505a69e2bbc934e3aa88a108c626307765
orakfarzaneh@gmail.com <orakfarzaneh@gmail.com>
2019-03-19 21:44:08 -0700

    go through array if number of misplaces are more than two then swap and check if the array is sorted

--- DetermineSortedArray/DetermineSortedArray/Program.cs
+++ DetermineSortedArray/DetermineSortedArray/Program.cs
@@ -7,7 +7,7 @@ namespace DetermineSortedArray
         static void Main(string[] args)
         {
             // Determine by swaping and removing one element from the array, the array would be sorted
-            int[] intArr = new int[] { 1, 10, 3, 4, 5 };
+            int[] intArr = new int[] { 5, 3, 4, 2 };
             bool isSorted = isArraySorted(intArr);
             Console.WriteLine(isSorted);
             Console.ReadLine();
@@ -25,16 +25,19 @@ namespace DetermineSortedArray
             {
                 if (intArr[i] > intArr[i+1])
                 {
+                    if (counter == 0)
+                    {
+                        left = i;
+                    }
+                    if (counter == 1)
+                    {
+                        right = i + 1;
+
+                    }
                     counter++;
+
                 }
 
-                if (counter == 2)
-                {
-                    left = i;
-                    right = i + 1;
-                }
-   
-                
             }
 
             if (counter == 1)
@@ -44,7 +47,23 @@ namespace DetermineSortedArray
 
             if (counter == 2)
             {
+                bool foundOne = false;
+                int temp = intArr[left];
+                intArr[left] = intArr[right];
+                intArr[right] = temp;
+                for (int i = 0; i < intArr.Length - 1; i++)
+                {
+                    if (intArr[i] > intArr[i + 1])
+                    {
+                        foundOne = true;
+                        break;
+                    }
+                }
+                if (!foundOne)
+                {
+                    isSorted = true;
 
+                }
             }
             if (counter > 3)
             {
