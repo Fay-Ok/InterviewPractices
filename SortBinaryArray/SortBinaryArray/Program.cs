e73bca94d2d5f87c4c4434467c160653061a4c8b
orakfarzaneh@gmail.com <orakfarzaneh@gmail.com>
2019-03-21 21:02:05 -0700

    Sorting binary array

---
+++ SortBinaryArray/SortBinaryArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortBinaryArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[] { 1, 0, 1, 1, 1, 1, 1, 0, 0, 0 };
+            sortArray(intArray);
+            Console.ReadLine();
+        }
+
+        private static void sortArray(int[] intArray)
+        {
+            List<int> arrayOfZerro = new List<int>() { };
+            List<int> arrayOfOne = new List<int>() { };
+            for (int i = 0; i < intArray.Length - 1 ; i++)
+            {
+                if (intArray[i] == 0)
+                {
+                    arrayOfZerro.Add(0);
+                }
+                else
+                {
+                    arrayOfOne.Add(1);
+                }
+                
+               
+            }
+            arrayOfZerro.AddRange(arrayOfOne);
+            for (int i = 0; i < arrayOfZerro.Count; i++)
+            {
+                Console.Write(arrayOfZerro[i] + " ");
+
+            }
+        }
+    }
+}
