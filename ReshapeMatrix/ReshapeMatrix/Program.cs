e00fd17dd2b078db07647f33df5bd0547641d5c2
orakfarzaneh@gmail.com <orakfarzaneh@gmail.com>
2019-04-03 22:52:59 -0700

    Reshape the matrix

--- ReshapeMatrix/ReshapeMatrix/Program.cs
+++ ReshapeMatrix/ReshapeMatrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ReshapeMatrix
 {
@@ -9,9 +10,11 @@ namespace ReshapeMatrix
             var arr = new int[,] {
                 { 1, 2},
                 { 3, 4 },
-             
+                { 5, 6 },
+                { 7, 8 },
+
             };
-            int x = 1;
+            int x = 2;
             int y = 4;
             ReshapeMatrix(arr, x, y);
             Console.ReadLine();
@@ -21,25 +24,26 @@ namespace ReshapeMatrix
         {
             int matrixSize = arr.Length;
             int[,] reshapedmatrix = new int[x,y];
+            Queue<int> q = new Queue<int>();
+
             if (x * y != matrixSize)
             {
                 return;
             }
-            int a = 0;
-            int b = 0;
             for (int i = 0; i < arr.GetLength(0); i++)
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                    reshapedmatrix[a, b] = arr[i, j];
-                    if (a < x -1 )
-                    {
-                        a++;
-                    }
-                    if (b < y -1)
-                    {
-                        b++;
-                    }
+                    q.Enqueue(arr[i, j]);
+                    
+                }
+            }
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                   reshapedmatrix[i,j]= q.Dequeue();
+
                 }
             }
         }
