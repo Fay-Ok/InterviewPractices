195afecc45e2c8870339df84a26ede72da416e5f
orakfarzaneh@gmail.com <orakfarzaneh@gmail.com>
2019-04-08 20:52:13 -0700

    Added project two sum. Find the indices in an array where sum of them is equal to the target

---
+++ TwoSumUsingHashMap/TwoSumUsingHashMap/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoSumUsingHashMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 2, 7, 11, 15 };
+            int target = 18;
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                dic.Add(arr[i], i);
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var compliment = target - arr[i];
+                if (dic.ContainsKey(compliment))
+                {
+                    Console.WriteLine("the indices of two numbers: [" + dic[compliment] +", " + i + "]");
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
