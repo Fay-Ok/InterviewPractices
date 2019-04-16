93c1f398022bcf437ec404405fd68fdd9de65a22
orakfarzaneh@gmail.com <orakfarzaneh@gmail.com>
2019-04-15 22:12:04 -0700

    Added a project- search insert position

---
+++ SearchInsertPosition/SearchInsertPosition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SearchInsertPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 3, 5, 6 };
+            int target = 5;
+            var result = FindInsertIndex(arr, target);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        private static int FindInsertIndex(int[] arr, int target)
+        {
+            int result = 0;
+            return result;
+        }
+    }
+}
