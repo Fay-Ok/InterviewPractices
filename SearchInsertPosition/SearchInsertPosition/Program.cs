9befb561818e87c8b1b4a9e8e74366b7bb1c7df2
orakfarzaneh@gmail.com <orakfarzaneh@gmail.com>
2019-04-17 21:31:27 -0700

    Find the index of a target in an array-completed

--- SearchInsertPosition/SearchInsertPosition/Program.cs
+++ SearchInsertPosition/SearchInsertPosition/Program.cs
@@ -6,8 +6,8 @@ namespace SearchInsertPosition
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[] { 1, 3, 5, 8 };
-            int target = 6;
+            int[] arr = new int[] { 1, 3 };
+            int target = 5;
             var result = FindInsertIndex(arr, target);
             Console.WriteLine(result);
             Console.ReadLine();
@@ -15,55 +15,26 @@ namespace SearchInsertPosition
 
         private static int FindInsertIndex(int[] arr, int target)
         {
-            int result = 0;
-            if (arr.Length == 0)
-            {
-                return 0;
-            }
-            int start = 0;
-            int end = arr.Length - 1;
-            if (target > arr[end])
-            {
-                result = end + 1;
-                return result;
-            }
 
-            if (target < arr[start])
+            int start = 0, end = arr.Length - 1;
+            while (start <= end)
             {
-                result = start;
-                return result;
-            }
-
-            while (start < end)
-            {
-                int mid =(end + start) / 2;
+                int mid = (start + end) / 2;
                 if (arr[mid] == target)
                 {
-                    result = mid;
-                    return result;
-                }
-                if (arr[mid] < target)
-                {
-                    if (target > arr[mid + 1])
-                    {
-                        result = mid;
-                        return result;
-                    }
-                    start = mid;
-                }
-                else 
-                {
-                    if (target > arr[mid - 1])
-                    {
-                        result = mid -1;
-                        return result;
-                    }
-                    end = mid;
-                }
+                    return mid;
 
-               
+                }
+                else if (arr[mid] > target)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
             }
-            return result;
+            return start;
         }
     }
 }
