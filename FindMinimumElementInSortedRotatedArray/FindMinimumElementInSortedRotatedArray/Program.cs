90eca2e54d3287338d07631ae3308274ba368b6a
orakfarzaneh@gmail.com <orakfarzaneh@gmail.com>
2019-04-18 22:25:55 -0700

    WIP

--- FindMinimumElementInSortedRotatedArray/FindMinimumElementInSortedRotatedArray/Program.cs
+++ FindMinimumElementInSortedRotatedArray/FindMinimumElementInSortedRotatedArray/Program.cs
@@ -6,7 +6,15 @@ namespace FindMinimumElementInSortedRotatedArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[] { 5, 6, 1, 2, 3, 4 };
+            int result = FindMin(args);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        private static int FindMin(string[] args)
+        {
+            throw new NotImplementedException();
         }
     }
 }
