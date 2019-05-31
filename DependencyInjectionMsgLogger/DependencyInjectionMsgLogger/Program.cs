c35c31e9b655daedfa51013a357ae6455dd62196
orakfarzaneh@gmail.com <orakfarzaneh@gmail.com>
2019-05-30 20:31:30 -0700

    Dependency injection message logger example

---
+++ DependencyInjectionMsgLogger/DependencyInjectionMsgLogger/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace DependencyInjectionMsgLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "DataBase ";
+
+            ILogger logger;
+
+            switch (msg)
+            {
+                case "File":
+                    logger = new TextFileLogger();
+                    break;
+
+                default:
+                    logger = new DatabaseLogger();
+                    break;
+            }
+
+            LogHelper logHelper = new LogHelper(logger);
+            logger.Log(msg);
+            Console.ReadLine();
+        }
+    }
+
+    interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class LogHelper
+    {
+        private ILogger _ILoger;
+        public LogHelper(ILogger iLogger)
+        {
+            _ILoger = iLogger;
+        }
+
+        public void Log(string message)
+        {
+            _ILoger.Log(message);
+        }
+
+    }
+    class TextFileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message + "Will be written in the text file");
+        }
+    }
+
+    class DatabaseLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message + "Will be written in Database");
+
+        }
+    }
+}
